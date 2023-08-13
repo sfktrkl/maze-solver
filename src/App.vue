a4df3197fe6a4ed1dbf2dfad846690e1c8070746
Safak Turkeli <sfktrkl@gmail.com>
2023-08-14 01:53:28 +0300

    Inital commit for Maze Solver

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HomePage />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import HomePage from "./components/HomePage.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    HomePage,
   },
 });
 </script>
 
 <style>
-#app {
+html,
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  margin: 0;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="title">Maze Solver</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomePage",
+});
+</script>
+
+<style scoped>
+.title {
+  text-align: center;
+  padding: 20px;
+  margin: auto;
+  width: 50%;
+}
+</style>
