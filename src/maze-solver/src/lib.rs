e6549fa0b4e2be9955982ea58782d1f6044f323d
Safak Turkeli <sfktrkl@gmail.com>
2023-08-22 03:12:31 +0300

    Refactor maze solver

--- src/maze-solver/src/lib.rs
+++ src/maze-solver/src/lib.rs
@@ -63,12 +63,53 @@ impl MazeSolver {
     pub fn new(height: usize, width: usize, row_count: usize, column_count: usize) -> Self {
         let cells = MazeSolver::generate_cells(height, width, row_count, column_count);
         let cells_js = vec![vec![None; column_count]; row_count];
-        MazeSolver { cells, cells_js, animation_settings: None }
+        MazeSolver {
+            cells,
+            cells_js,
+            animation_settings: None,
+        }
+    }
+
+    fn generate_cells(
+        height: usize,
+        width: usize,
+        row_count: usize,
+        column_count: usize,
+    ) -> Vec<Vec<Vec<f32>>> {
+        let x = width as f32 / column_count as f32;
+        let y = height as f32 / row_count as f32;
+
+        let mut cells = Vec::new();
+        for i in 0..row_count {
+            let mut row = Vec::new();
+            for j in 0..column_count {
+                row.push(vec![
+                    j as f32 * x,
+                    i as f32 * y,
+                    (j + 1) as f32 * x,
+                    (i + 1) as f32 * y,
+                ]);
+            }
+            cells.push(row);
+        }
+        cells
     }
 
     #[wasm_bindgen]
     pub fn set_animation_settings(&mut self, animation_settings: AnimationSettings) {
         self.animation_settings = Some(animation_settings)
+    }
+
+    fn maze_animation(&self) -> bool {
+        self.animation_settings
+            .as_ref()
+            .map_or(true, |settings| settings.get_maze_animation())
+    }
+
+    fn solver_animation(&self) -> bool {
+        self.animation_settings
+            .as_ref()
+            .map_or(true, |settings| settings.get_solver_animation())
     }
 
     #[wasm_bindgen]
@@ -83,14 +124,9 @@ impl MazeSolver {
 
     #[wasm_bindgen]
     pub async fn break_entrance_and_exit(&mut self) {
-        let mut animate = true;
-        if let Some(settings) = &self.animation_settings {
-            animate = settings.get_maze_animation();
-        }
-
         if let Some(cell) = &self.cells_js[0][0] {
             cell.set_top_wall(false);
-            cell.draw(animate).await;
+            cell.draw(self.maze_animation()).await;
         }
 
         let last_row = self.cells_js.len() - 1;
@@ -98,13 +134,63 @@ impl MazeSolver {
 
         if let Some(cell) = &self.cells_js[last_row][last_col] {
             cell.set_bottom_wall(false);
-            cell.draw(animate).await;
+            cell.draw(self.maze_animation()).await;
         }
     }
 
     #[wasm_bindgen]
     pub async fn break_walls(&mut self) {
         self.break_walls_iterative(0, 0).await;
+    }
+
+    async fn break_walls_iterative(&mut self, start_i: usize, start_j: usize) {
+        let mut stack = vec![(start_i, start_j)];
+        let row_count = self.cells_js.len();
+        let column_count = self.cells_js[0].len();
+        let directions = vec![(0, -1), (-1, 0), (0, 1), (1, 0)];
+        while let Some((i, j)) = stack.pop() {
+            if let Some(cell) = &self.cells_js[i][j] {
+                cell.set_visited(true);
+                let mut next_index_list = vec![];
+
+                for &(delta_i, delta_j) in directions.iter() {
+                    let next_i = (i as isize).wrapping_add(delta_i) as usize;
+                    let next_j = (j as isize).wrapping_add(delta_j) as usize;
+                    if next_i < row_count && next_j < column_count {
+                        if let Some(next_cell) = &self.cells_js[next_i][next_j] {
+                            if !next_cell.get_visited() {
+                                next_index_list.push((next_i, next_j));
+                            }
+                        }
+                    }
+                }
+
+                if next_index_list.is_empty() {
+                    cell.draw(self.maze_animation()).await;
+                } else {
+                    let mut rng = rand::thread_rng();
+                    let random_index = rng.gen_range(0..next_index_list.len());
+                    let (next_i, next_j) = next_index_list[random_index];
+                    if let Some(next_cell) = &self.cells_js[next_i][next_j] {
+                        if i > next_i {
+                            cell.set_top_wall(false);
+                            next_cell.set_bottom_wall(false);
+                        } else if i < next_i {
+                            cell.set_bottom_wall(false);
+                            next_cell.set_top_wall(false);
+                        } else if j > next_j {
+                            cell.set_left_wall(false);
+                            next_cell.set_right_wall(false);
+                        } else if j < next_j {
+                            cell.set_right_wall(false);
+                            next_cell.set_left_wall(false);
+                        }
+                    }
+                    stack.push((i, j));
+                    stack.push((next_i, next_j));
+                }
+            }
+        }
     }
 
     #[wasm_bindgen]
@@ -132,23 +218,19 @@ impl MazeSolver {
         while let Some((path, i, j)) = stack.pop() {
             if let Some(cell) = &self.cells_js[i][j] {
                 cell.set_visited(true);
-                let mut animate = true;
                 if let Some(prev_cell) = &path.last().unwrap() {
-                    if let Some(settings) = &self.animation_settings {
-                        animate = settings.get_solver_animation();
-                    }
-                    prev_cell.draw_move(cell, false, animate).await;
+                    prev_cell
+                        .draw_move(cell, false, self.solver_animation())
+                        .await;
                 }
 
                 if i == row_count - 1 && j == column_count - 1 {
                     let mut current = cell;
                     for prev in path.iter().rev() {
                         if let Some(prev_cell) = &prev {
-                            let mut animate = true;
-                            if let Some(settings) = &self.animation_settings {
-                                animate = settings.get_solver_animation();
-                            }
-                            current.draw_move(prev_cell, true, animate).await;
+                            current
+                                .draw_move(prev_cell, true, self.solver_animation())
+                                .await;
                             current = prev_cell;
                         }
                     }
@@ -177,114 +259,6 @@ impl MazeSolver {
         }
 
         false
-    }
-
-    async fn break_walls_iterative(&mut self, start_i: usize, start_j: usize) {
-        let mut stack = vec![(start_i, start_j)];
-        let row_count = self.cells_js.len();
-        let column_count = self.cells_js[0].len();
-        while let Some((i, j)) = stack.pop() {
-            if let Some(cell) = &self.cells_js[i][j] {
-                cell.set_visited(true);
-
-                let mut next_index_list = vec![];
-                if i > 0 {
-                    if let Some(top) = &self.cells_js[i - 1][j] {
-                        if !top.get_visited() {
-                            next_index_list.push((i - 1, j));
-                        }
-                    }
-                }
-                if i < row_count - 1 {
-                    if let Some(bottom) = &self.cells_js[i + 1][j] {
-                        if !bottom.get_visited() {
-                            next_index_list.push((i + 1, j));
-                        }
-                    }
-                }
-                if j > 0 {
-                    if let Some(left) = &self.cells_js[i][j - 1] {
-                        if !left.get_visited() {
-                            next_index_list.push((i, j - 1));
-                        }
-                    }
-                }
-                if j < column_count - 1 {
-                    if let Some(right) = &self.cells_js[i][j + 1] {
-                        if !right.get_visited() {
-                            next_index_list.push((i, j + 1));
-                        }
-                    }
-                }
-
-                if next_index_list.is_empty() {
-                    let mut animate = true;
-                    if let Some(settings) = &self.animation_settings {
-                        animate = settings.get_maze_animation();
-                    }
-                    cell.draw(animate).await;
-                } else {
-                    let mut rng = rand::thread_rng();
-                    let random_index = rng.gen_range(0..next_index_list.len());
-                    let (next_i, next_j) = next_index_list[random_index];
-
-                    if let Some(cell) = &self.cells_js[i][j] {
-                        if i > 0 && next_i == i - 1 {
-                            cell.set_top_wall(false);
-                            if let Some(top) = &self.cells_js[next_i][j] {
-                                top.set_bottom_wall(false);
-                            }
-                        }
-                        if next_i == i + 1 {
-                            cell.set_bottom_wall(false);
-                            if let Some(bottom) = &self.cells_js[next_i][j] {
-                                bottom.set_top_wall(false);
-                            }
-                        }
-                        if j > 0 && next_j == j - 1 {
-                            cell.set_left_wall(false);
-                            if let Some(left) = &self.cells_js[i][next_j] {
-                                left.set_right_wall(false);
-                            }
-                        }
-                        if next_j == j + 1 {
-                            cell.set_right_wall(false);
-                            if let Some(right) = &self.cells_js[i][next_j] {
-                                right.set_left_wall(false);
-                            }
-                        }
-                    }
-
-                    stack.push((i, j));
-                    stack.push((next_i, next_j));
-                }
-            }
-        }
-    }
-
-    fn generate_cells(
-        height: usize,
-        width: usize,
-        row_count: usize,
-        column_count: usize,
-    ) -> Vec<Vec<Vec<f32>>> {
-        let x = width as f32 / column_count as f32;
-        let y = height as f32 / row_count as f32;
-
-        let mut cells = Vec::new();
-        for i in 0..row_count {
-            let mut row = Vec::new();
-            for j in 0..column_count {
-                let mut cell = Vec::new();
-                cell.push(j as f32 * x);
-                cell.push(i as f32 * y);
-                cell.push((j + 1) as f32 * x);
-                cell.push((i + 1) as f32 * y);
-                row.push(cell);
-            }
-            cells.push(row);
-        }
-        cells
     }
 }
 
